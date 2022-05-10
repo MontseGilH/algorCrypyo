011b8728b1513ae094f3dfdfce5a95b62291cfb8
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-05-09 20:21:16 -0600

    Update algrocrypt.py

--- algrocrypt.py
+++ algrocrypt.py
@@ -1,12 +1,20 @@
-def llavePublica(p,q):
+"""
+Programa para encriptar mensajes
+Matematica discreta
+Programado por: Montserrat Gil
+Fecha 9/05/2022
+"""
+
+# p y q son los dos primos
+# si se tiene un valor de e, escribirlo, sino, false
+def llavePublica(p,q,e1):
     n=p*q
     phi=(p-1)*(q-1)
-    si=False
-    e=2
-    while not si:
-        if phi%e==0:
-            e+=1
-        else:
-            si=True
+    if not e1:
+        
+    else: 
+        e=e1
     return n,e
 
+def encriptar(n,e,string):
+    
