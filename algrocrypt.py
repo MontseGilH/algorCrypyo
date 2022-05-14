c024017e206db1f825cd82922343c00d91541603
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-05-14 10:23:13 -0600

    terminar el alg crypto

--- algrocrypt.py
+++ algrocrypt.py
@@ -7,8 +7,7 @@ Fecha 9/05/2022
 
 # p y q son los dos primos
 # si se tiene un valor de e, escribirlo, sino, false
-def llavePublica(p,q,e1):
-    n=p*q
+def crearEyD(p,q,e1):
     phi=(p-1)*(q-1)
     if not e1:
         e=2
@@ -16,7 +15,11 @@ def llavePublica(p,q,e1):
             e+=1
     else: 
         e=e1
-    return n,e
+    d=0
+    for i in range(phi):
+        if (i*e)%phi==1:
+            d=i     
+    return e,d
 
 def mcd(num1,num2):
     fin=False
@@ -29,37 +32,6 @@ def mcd(num1,num2):
             temp1=num1
             num1=num2
             num2=temp1-(division*num2)
-        
-
-def encriptar(n,e,string):
-    letras= {
-        " ":"00","a":"01","b":"02","c":"03","d":"04","e":"05","f":"06","g":"07","h":"08","i":"09","j":"10","k":"11","l":"12","m":"13","n":"14","ñ":"15","o":"16","p":"17","q":"18","r":"19","s":"20","t":"21","u":"22","v":"23","w":"24","x":"25","y":"26","z":"27",
-    }
-    mensajeNum=""
-    for s in string.lower():
-        mensajeNum+=letras[s]
-    b=0
-    lim="27"
-    while int(lim)<n:
-        b+=2
-        lim+="27"
-    while not (len(mensajeNum)%b)==0:
-        mensajeNum+="00"
-    lista=[]
-    start=0
-    for i in range(int(len(mensajeNum)/b)):
-        lista.append(mensajeNum[start:start+b])
-        start=start+b
-    for l in range(len(lista)):
-        cr1=int(lista[l])
-        c=str(mod(cr1,e,n))
-        while len(c)<4:
-            c="0"+c
-        lista[l]=c
-    mensajeEntript=""
-    for k in lista:
-        mensajeEntript+=k+" "
-    return mensajeEntript
 
 def mod(n,p,m):
     if p==0:
@@ -69,13 +41,61 @@ def mod(n,p,m):
     else:
         return (n*mod(n,(p-1)/2,m)**2)%m
 
-print(encriptar(2999,3,"hola"))
+def b(n):
+    b=0
+    lim="27"
+    while int(lim)<n:
+        b+=2
+        lim+="27"
+    return b
+            
+def encriptar(n,e,string,b):
+    letras= {
+        " ":"00","a":"01","b":"02","c":"03","d":"04","e":"05","f":"06","g":"07","h":"08","i":"09","j":"10","k":"11","l":"12","m":"13","n":"14","ñ":"15","o":"16","p":"17","q":"18","r":"19","s":"20","t":"21","u":"22","v":"23","w":"24","x":"25","y":"26","z":"27",
+    }
+    mensajeNum=""
+    for s in string.lower():
+        mensajeNum+=letras[s]
+    while not (len(mensajeNum)%b)==0:
+        mensajeNum+="00"
+    mensajeEntript=""
+    for i in range(int(len(mensajeNum)/b)):
+        cr1=int(mensajeNum[b*i:b+i*b])
+        c=str(mod(cr1,e,n))
+        while len(c)<b:
+            c="0"+c
+        mensajeEntript+=c+" "
+    return mensajeEntript
 
-def inverso(num1,mod):
-    for i in range(mod):
-        if (i*num1)%mod==1:
-            return i
+def desencriptar(d,n,mensaje,b):
+    letras=[" ","a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
+    bloques=mensaje.split()
+    final=""
+    for bloque in bloques:
+        res = str(mod(int(bloque),d,n))
+        while len(res)<b:
+            res="0"+res
+        for i in range(int(b/2)):
+            numl=int(res[2*i:2+2*i])
+            final+=letras[numl]
+    return final  
 
-def decript(mensaje,n,e):
-    d=inverso(e,n)
-print(inverso(13,2436))
+class Encriptor:
+    
+    def __init__(self,p,q,e1):
+        self.n=p*q
+        self.e,self.d=crearEyD(p,q,e1)
+        self.b=b(self.n)
+    
+    def encript(self,string):
+        return encriptar(self.n,self.e,string,self.b)
+    
+    def desencript(self,string):
+        return desencriptar(self.d,self.n,string,self.b)
+
+encr=Encriptor(43,59,False)
+mensEncr=encr.encript("hola como estas maria")
+mensajeDesencr=encr.desencript(mensEncr)
+print(mensEncr)
+print(mensajeDesencr)
+
